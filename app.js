68b8c732438469ba826d45837cccf01d09d4b50c
Edson Famanda <edson@equipmoz.org>
2021-04-03 22:14:10 +0200

    Outro filme adicionado

--- app.js
+++ app.js
@@ -8,12 +8,20 @@ const app = {
                 title: 'SmallVille',
                 description: 'Filme muito interessante',
                 image: './assets/img/img3.jpg',
+                stars: 3,
             },
             product2: {
+                title: 'Filme 03',
+                description: 'Segundo filme na lista',
+                image: './assets/img/img1.jpg',
+                stars: 5,
+            },
+            product3: {
                 title: 'Filme 02',
                 description: 'Outro filme interessante',
                 image: './assets/img/img2.jpg',
-            }
+                stars: 0,
+            },
         }
     }
 
