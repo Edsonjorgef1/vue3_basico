3af1ab2fe9187d9df4e8622f7c1bc1d566069a15
Edson Famanda <edson@equipmoz.org>
2021-04-03 22:03:19 +0200

    Retornando dados usando v-bind

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const app = {
         return {
             name: 'Edson',
             lastName: 'Francisco',
+            product: {
+                title: 'Filme 01',
+                description: 'Filme muito interessante',
+                image: './assets/img/img3.jpg',
+            }
         }
     }
 
 }
+
 Vue.createApp(app).mount('#app')
