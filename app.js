a2b9d31e3f3ac9e751713ddb3a05e801f8daa3be
Edson Famanda <edson@equipmoz.org>
2021-04-03 22:09:40 +0200

    Outro filme adicionado e apresentando usando as propriedades de v-bind

--- app.js
+++ app.js
@@ -5,9 +5,14 @@ const app = {
             name: 'Edson',
             lastName: 'Francisco',
             product: {
-                title: 'Filme 01',
+                title: 'SmallVille',
                 description: 'Filme muito interessante',
                 image: './assets/img/img3.jpg',
+            },
+            product2: {
+                title: 'Filme 02',
+                description: 'Outro filme interessante',
+                image: './assets/img/img2.jpg',
             }
         }
     }
