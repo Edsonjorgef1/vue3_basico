c8542cb470d4e7008a81e5394461faa5781a1af2
Edson Famanda <edson@equipmoz.org>
2021-04-10 23:41:57 +0200

    Adding in cart - Chamando o metodo pelo componente

--- app.js
+++ app.js
@@ -77,8 +77,19 @@ const productDetail = {
     template: ` <p> {{product.title}}</p>
                     <img :src="product.image" :alt="product.title" style="max-width: 500px; max-height: 500px;">
                     <p> <strong v-if="product.stars > 0"> {{ product.stars }}&#9734; </strong> {{product.description}}</p>
+                    <button
+                            @click.prevent="addCart(product)"
+                            :disabled="productInCart"
+                            :class="{'button-disabled' : productInCart }">
+                        ADD CART
+                    </button>
     `,
-    props: ['product'],
+    props: ['product', 'productInCart'],
+    methods: {
+        addCart(product){
+            console.log('Add cart in component')
+        }
+    },
 
     data(){
         return {
