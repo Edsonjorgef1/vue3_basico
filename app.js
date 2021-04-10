ef2331d194216bd2276110f95fbb5828f274c077
Edson Famanda <edson@equipmoz.org>
2021-04-10 23:47:58 +0200

    Separando os components em arquivos separados

--- app.js
+++ app.js
@@ -72,33 +72,4 @@ const myApp = {
     }
 }
 
-// Objecto que vai retornar a informacao no componente
-const productDetail = {
-    template: ` <p> {{product.title}}</p>
-                    <img :src="product.image" :alt="product.title" style="max-width: 500px; max-height: 500px;">
-                    <p> <strong v-if="product.stars > 0"> {{ product.stars }}&#9734; </strong> {{product.description}}</p>
-                    <button
-                            @click.prevent="addCart(product)"
-                            :disabled="productInCart"
-                            :class="{'button-disabled' : productInCart }">
-                        ADD CART
-                    </button>
-    `,
-    props: ['product', 'productInCart'],
-    methods: {
-        addCart(product){
-            console.log('Add cart in component')
-        }
-    },
-
-    data(){
-        return {
-            example: 123,
-        }
-    }
-}
-
 const app = Vue.createApp(myApp)
-
-app.component('product-detail', productDetail)  // Registrando o componente
-app.mount('#app')
